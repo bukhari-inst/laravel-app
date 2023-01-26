e45dbeff31d93538956df33e7abfba0daebdbcc1
Bukhari_Inst <bukhari414647@gmail.com>
2023-01-26 10:49:03 +0700

    layouts, controller

---
+++ resources/views/index.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.main')
+
+@section('home')
+    <h2>Home</h2>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Home;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +15,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('welcome', [
+        "documentation" => "Documentation"
+    ]);
 });
+
+Route::get('/index', [Home::class, 'index']);
